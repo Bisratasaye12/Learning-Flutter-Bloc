6a65dc405b44daab3236d5c2db004bab6bba7c25
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-07-09 23:09:39 +0300

    implemented the decrement functionality

--- counter_app_with_cubit/lib/home_page.dart
+++ counter_app_with_cubit/lib/home_page.dart
@@ -17,35 +17,46 @@ class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: Text(widget.title),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  BlocBuilder<CounterCubit, int>(
-                    bloc: counterCubit,
-                    builder: (context, counter) {
-                      return Text(
-                        '$counter',
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      );
-                    }
-                  ),
-                ],
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: counterCubit.increment,
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
-          );
-        
+            BlocBuilder<CounterCubit, int>(
+                bloc: counterCubit,
+                builder: (context, counter) {
+                  return Text(
+                    '$counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: counterCubit.increment,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          FloatingActionButton(
+            onPressed: counterCubit.decrement,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
